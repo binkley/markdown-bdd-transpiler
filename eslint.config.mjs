b1c91de906c45243b63b5dd7ccef8f7bdfd33abc
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2026-05-18 11:13:59 -0500

    fix: eslint config for node and browser globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,19 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import globals from 'globals';
 
-export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'off'
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser
+      }
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off'
+    }
   }
-});
+);
